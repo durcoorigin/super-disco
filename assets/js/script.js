295888a8d63d1c2988b388dc4ceacc37fb4eeb42
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-04-18 13:04:21 -0500

    update text entry and add unique id to save button

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$('.task').each(function() {
+    var default_value = this.value;
+    $(this).focus(function() {
+        if(this.value == default_value) {
+            this.value = '';
+        }
+    });
+    $(this).blur(function() {
+        if(this.value == '') {
+            this.value = default_value;
+        }
+    });
+});
+
+function addTask(btnId) {
+    $('#0000').append($('#task0').val());
+    console.log(btnId);
+}
