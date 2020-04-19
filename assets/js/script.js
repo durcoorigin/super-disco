6d918f96684f5690a8d5f84514942438ba4fc279
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-04-19 12:27:23 -0500

    create functions to save tasks to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,100 @@
-$('.task').each(function() {
-    var default_value = this.value;
-    $(this).focus(function() {
-        if(this.value == default_value) {
-            this.value = '';
-        }
-    });
-    $(this).blur(function() {
-        if(this.value == '') {
-            this.value = default_value;
-        }
-    });
-});
 
-function addTask(btnId) {
-    $('#0000').append($('#task0').val());
-    console.log(btnId);
+// ****** 8 ******
+function saveTask8(btnId8,taskId8) {
+    localStorage.setItem('newTask8', taskId8);
 }
+
+function loadTask8(taskId8) {
+    document.getElementById('task8').innerHTML = localStorage.getItem('newTask8');
+}
+loadTask8();
+
+// ****** 9 ******
+function saveTask9(btnId9,taskId9) {
+    localStorage.setItem('newTask9', taskId9);
+}
+
+function loadTask9(taskId9) {
+    document.getElementById('task9').innerHTML = localStorage.getItem('newTask9');
+}
+loadTask9();
+
+// ****** 10 ******
+function saveTask10(btnId10,taskId10) {
+    localStorage.setItem('newTask10', taskId10);
+}
+
+function loadTask10(taskId10) {
+    document.getElementById('task10').innerHTML = localStorage.getItem('newTask10');
+}
+loadTask10();
+
+// ****** 11 ******
+function saveTask11(btnId11,taskId11) {
+    localStorage.setItem('newTask11', taskId11);
+}
+
+function loadTask11(taskId11) {
+    document.getElementById('task11').innerHTML = localStorage.getItem('newTask11');
+}
+loadTask11();
+
+// ****** 12 ******
+function saveTask12(btnId12,taskId12) {
+    localStorage.setItem('newTask12', taskId12);
+}
+
+function loadTask12(taskId12) {
+    document.getElementById('task12').innerHTML = localStorage.getItem('newTask12');
+}
+loadTask12();
+
+// ****** 13 ******
+function saveTask13(btnId13,taskId13) {
+    localStorage.setItem('newTask13', taskId13);
+}
+
+function loadTask13(taskId13) {
+    document.getElementById('task13').innerHTML = localStorage.getItem('newTask13');
+}
+loadTask13();
+
+// ****** 14 ******
+function saveTask14(btnId14,taskId14) {
+    localStorage.setItem('newTask14', taskId14);
+}
+
+function loadTask14(taskId14) {
+    document.getElementById('task14').innerHTML = localStorage.getItem('newTask14');
+}
+loadTask14();
+
+// ****** 15 ******
+function saveTask15(btnId15,taskId15) {
+    localStorage.setItem('newTask15', taskId15);
+}
+
+function loadTask15(taskId15) {
+    document.getElementById('task15').innerHTML = localStorage.getItem('newTask15');
+}
+loadTask15();
+
+// ****** 16 ******
+function saveTask16(btnId16,taskId16) {
+    localStorage.setItem('newTask16', taskId16);
+}
+
+function loadTask16(taskId16) {
+    document.getElementById('task16').innerHTML = localStorage.getItem('newTask16');
+}
+loadTask16();
+
+// ****** 17 ******
+function saveTask17(btnId17,taskId17) {
+    localStorage.setItem('newTask17', taskId17);
+}
+
+function loadTask17(taskId17) {
+    document.getElementById('task17').innerHTML = localStorage.getItem('newTask17');
+}
+loadTask17();
